88c5098d81963987aa4f8b1c7cdd1964011016e0
Marvin Artiga AG230133 <marvin.artigagonzalez@gmail.com>
2024-03-01 17:00:59 -0600

    agregar

---
+++ ejercicio2/src/app/data.js
@@ -0,0 +1,83 @@
+export const data=[
+    {
+    id:1,
+    title:'Camara sony',
+    price:100,
+    description:'Camara de video (sony)',
+    urlImage:'https://i.blogs.es/8ccac1/hdr-xr105e_a/450_1000.jpg',
+    quantity:1
+    },
+    {
+    id:2,
+    title:'Camara panasonic',
+    price:100,
+    description:'Camara de video (Panasonic)',
+    urlImage:'https://www.fotocasion.es/media/img/articulos/55429.jpg',
+    quantity:1
+    },
+    {
+    id:3,
+    title:'Bocinas RGB',
+    price:150,
+    description:'Bocinas RGB',   
+    urlImage:'https://www.zonadigitalsv.com/product/798302164444/image',
+    quantity:1
+    },
+    {
+    id:4,
+    title:'Bocinas Bluetooth',
+    price:150,
+    description:'Bocinas Bluetooth pequeñas',   
+     urlImage:'https://www.officedepot.com.mx/medias/100103568.jpg-1200ftw?context=bWFzdGVyfHJvb3R8NDg3NzYwfGltYWdlL2pwZWd8YURkaEwyaGpZeTh4TVRJMU1qZzJOVGN5T0RVME1pOHhNREF4TURNMU5qZ3VhbkJuWHpFeU1EQm1kSGN8NDIyNmNiNzk1NzEyMTM0NmRkMGJmY2Q5MTA2NTlkNmI2MDY4MTM3Y2QwNDkxOGUwNTYyNWJiOTZhOTI1Y2Q5Ng',
+    quantity:1
+    }, 
+    {
+    id:5,
+    title:'PACK Cautin Estaño',
+    price:200,
+    description:'Cautin (80W) + Estaño(30G)',
+    urlImage:'https://disparo.cl/wp-content/uploads/2023/05/8617-pack-cautin-electrico-tipo-lapi-para-soldar-80w-soldadura_1.jpg',
+    quantity:1
+},
+{
+    id:6,
+    title:'Arduino',
+    price:200,
+    description:'Placa Arduino UNO',
+    urlImage:'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Arduino_Uno_-_R3.jpg/250px-Arduino_Uno_-_R3.jpg',
+    quantity:1
+    },
+    {
+        id:7,
+        title:'Refrigerador',
+        price:250,
+        description:'Refrigerador Inteligente',   
+        urlImage:'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1637716442-15368505.jpg?crop=1xw:1.00xh;center,top&resize=980:*',
+        quantity:1
+        },
+        {
+            id:9,
+            title:'Smart TV',
+            price:250,
+            description:'Televisor inteligente Samsung',    
+            urlImage:'https://siman.vtexassets.com/arquivos/ids/4131772/104124145.jpg?v=638168458678170000',
+            quantity:1
+            },
+    {
+    id:8,
+    title:'Sofa',
+    price:300,
+    description:'Sofa',
+    urlImage:'https://siman.vtexassets.com/arquivos/ids/4242398-500-auto?v=638192881830000000&width=500&height=auto&aspect=true',
+    quantity:1
+    },
+ 
+    {
+    id:10,
+    title:'Escritorio Gamer',
+    price:300,
+    description:'Escritorio Gamer 120x55x76 ',
+    urlImage:'https://arcencohogar.vtexassets.com/arquivos/ids/313915-800-800?v=637794251828500000&width=800&height=800&aspect=true',
+    quantity:1
+}
+];
